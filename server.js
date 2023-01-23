5e2b911dd18585fc160ba2172771b7b2e2e78b0a
Nick Danelia <nikdanelia@gmail.com>
2023-01-24 00:13:53 +0400

    Initialized socket.io

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
 dotenv.config();
 const PORT = process.env.PORT;
 const DBURL = process.env.DB_URL.replace("<password>", process.env.DB_PASSWORD);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 const userRoutes = require("./routes/user");
 
-const app = express();
+io.on("connection", (socket) => {
+  console.log("OOPAAAA");
+  socket.emit("hello from server", "zdddd");
 
+  // receive a message from the client
+  socket.on("hello from client", (...args) => {
+    // ...
+  });
+  socket.on("hello", (...args) => {
+    console.log(args);
+  });
+});
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -27,6 +40,6 @@ mongoose.connect(DBURL, () => {
   console.log("Connected to DB!");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
